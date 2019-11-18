e0eac34717ab39c68cf32362e5cc89436cd236fc
Jaison Schmidt <jaison.schmidt@compasso.com.br>
2019-11-18 13:00:45 -0300

    First commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Welcome to Counter</h2>
+    <button @click="incrementCounter">increment counter</button>
+    <h2>{{counter}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        incrementCounter () {
+            this.$store.dispatch('incrementAction', 1)
+        }
+    },
+    computed: {
+      counter () {
+        return this.$store.getters.counter
+      }
+    }
+}
+</script>
+
+<style>
+</style>
